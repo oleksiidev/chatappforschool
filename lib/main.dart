cdaa884b8a0e71bf0a9b7482527fc921d94b9734
oleksiidev <oleksii.sydorenko.dev@gmail.com>
2021-12-01 19:39:19 +0100

    screens dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chatappforschool/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -7,14 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chatter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(child: Text("Hello World"),),
-      ),
-    );
+    return const MaterialApp(title: 'Chatter', home: HomeScreen());
   }
 }
